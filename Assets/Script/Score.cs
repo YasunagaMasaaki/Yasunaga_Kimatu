3fa3ea46bd9b2cc9459a7fc559f4786d0c94d649
YasunagaMasaaki <game22403016@gmail.com>
2025-02-13 15:12:50 +0900

    y

--- Assets/Script/Score.cs
+++ Assets/Script/Score.cs
@@ -2,14 +2,52 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using System.IO;
 
 public class Score : MonoBehaviour
 {
     public Text scoreText;
 
+    private string filepath;
+    private GameSaveData saveData;
+
+    private void Awake()
+    {
+        filepath = Path.Combine(Application.dataPath, "data.json");
+        LoadData();
+    }
+
+    private void LoadData()
+    {
+        if (File.Exists(filepath))
+        {
+            string json = File.ReadAllText(filepath);
+            saveData = JsonUtility.FromJson<GameSaveData>(json);
+        }
+        else
+        {
+            saveData = new GameSaveData();
+        }
+    }
+
+    private void SaveData()
+    {
+        string json = JsonUtility.ToJson(saveData, true);
+        File.WriteAllText(filepath, json);
+    }
+
     void Start()
     {
         int finalScore = PlayerPrefs.GetInt("FinalScore", 0); // スコアを取得
         scoreText.text = "Score: " + finalScore; // UIに表示
+        saveData.save1 = finalScore;
+        SaveData();
+        Debug.Log("data saved1");
+    }
+
+    [System.Serializable]
+    public class GameSaveData
+    {
+        public int save1 = 0;
     }
 }
